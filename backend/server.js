1f7d6c26d143e4567ca3c780b28f8f56cda856bb
JANANICODER <vjanani812@gmail.com>
2025-04-29 10:08:14 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,19 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+console.log("MONGO_URI:", process.env.MONGO_URI);  // Check if the URI is loaded correctly
+
 
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));  // Corrected to 'dist'
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));  // Corrected to 'dist'
 	});
-}
-
+  }
+  
 app.listen(PORT, () => {
 	connectDB();
 	console.log("Server started at http://localhost:" + PORT);
